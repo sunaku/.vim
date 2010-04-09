ba6ce19569fffe0da572d901f1b6e265ca9bf189
Suraj N. Kurapati <sunaku@gmail.com>
2010-04-08 21:28:49 -0700

    add get.rb script for simple Vim script updating

---
+++ bundle/get.rb
@@ -0,0 +1,130 @@
+#!/usr/bin/env ruby
+#
+# Updates Vim scripts (defined by *.get files in this directory) as needed.
+#
+# These *.get files are YAML documents that define a "script_id" parameter:
+#
+#   script_id: NNNNN
+#
+# where NNNNN is a placeholder for the unique identifier of the particular
+# Vim script that you want to download from http://www.vim.org/scripts/.
+#
+# The basenames of these *.get files (minus the file extension) are used as
+# directory names for housing their corresponding Vim scripts after download.
+#
+# After a new version of a Vim script is downloaded, the corresponding *.get
+# file is overwritten (while preserving the original "script_id" parameter) to
+# reflect the newly downloaded version, and the old download directory (if it
+# exists) is renamed to include its corresponding, old version identifier.
+#
+# This script was inspired by the GetLatestVimScripts utility:
+# http://www.vim.org/scripts/script.php?script_id=642
+#
+###
+#
+# Prerequisites:
+#
+# - Ruby 1.9 or newer      : http://www.ruby-lang.org
+# - mechanize 1.0 or newer : gem install mechanize
+# - unzip 6.0 or newer     : http://www.info-zip.org
+# - tar 1.23 or newer      : http://www.gnu.org/software/tar/
+#
+###
+#
+# (the ISC license)
+#
+# Copyright 2010 Suraj N. Kurapati <sunaku@gmail.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+
+require 'mechanize'
+require 'rake'
+require 'tmpdir'
+require 'yaml'
+
+Dir['*.get'].each do |get_file|
+  puts '', get_file
+  get_info = YAML.load_file(get_file)
+
+  script_id = get_info['script_id'] or
+  raise "#{get_file} must define the 'script_id' parameter"
+
+  # check for new version
+  agent = Mechanize.new
+  page = agent.get("http://www.vim.org/scripts/script.php?script_id=#{script_id}")
+
+  newest_version_link = page.links.find do |link|
+    link.href.start_with? 'download_script.php?src_id='
+  end or raise "no versions found for script_id=#{script_id}"
+
+  newest_src_id = newest_version_link.href[/\d+$/].to_i
+  current_src_id = get_info['src_id'].to_i
+
+  if newest_src_id > current_src_id
+    puts "#{get_file} is out of date (current=#{current_src_id}, newest=#{newest_src_id})"
+
+    Dir.mktmpdir do |download_dir|
+      # download new version
+      download = agent.click(newest_version_link)
+      download_file = File.join(download_dir, download.filename)
+      File.open(download_file, 'wb') {|f| f << download.body }
+
+      # extract new version
+      mkdir extract_dir = File.join(download_dir, '__extract__')
+
+      case download.filename
+      when /vim$/
+        mkdir plugin_dir = File.join(extract_dir, 'plugin')
+        mv download_file, plugin_dir
+
+      when /zip$/
+        sh 'unzip', '-d', extract_dir, download_file
+
+      when /tar$/
+        sh 'tar', '-x', '-C', extract_dir, '-f', download_file
+
+      when /tar\.gz$/, /tgz$/
+        sh 'tar', '-x', '-C', extract_dir, '-f', download_file, '--gzip'
+
+      when /tar\.bz2?$/, /tbz$/
+        sh 'tar', '-x', '-C', extract_dir, '-f', download_file, '--bzip2'
+
+      else
+        raise "don't know how to extract #{download.filename}"
+      end
+
+      # install new version
+      get_dir = File.expand_path(get_file).sub(/\.get$/, '')
+
+      if File.exist? get_dir
+        mv get_dir, [get_dir, current_src_id].join('@')
+      end
+
+      mv extract_dir, get_dir
+
+      # index help topics
+      doc_dir = File.join(get_dir, 'doc')
+
+      if File.directory? doc_dir
+        sh "vim -u NONE -c 'helptags #{doc_dir.inspect}' -c quit"
+      end
+    end
+
+    # update the get file to reflect the newly installed version
+    get_info['src_id'] = newest_src_id
+    File.open(get_file, 'w') {|f| f << get_info.to_yaml }
+  end
+
+  puts "#{get_file} is up to date"
+end
