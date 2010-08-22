ea72a6a90c6f0262324620d4f83844859d8fae7c
Suraj N. Kurapati <sunaku@gmail.com>
2010-08-22 00:35:53 -0700

    do not rely on default `git submodule` behavior

--- bundle.sh
+++ bundle.sh
@@ -2,7 +2,7 @@
 # Installs or upgrades all bundles defined in bundle/ as necessary.
 
 git submodule init
-git submodule | awk '/^-/ { print $2 }' | xargs -r git submodule update
+git submodule status | awk '/^-/ { print $2 }' | xargs -r git submodule update
 git submodule foreach git pull origin master
 
 cd bundle/
