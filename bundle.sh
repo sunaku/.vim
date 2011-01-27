26257a8b640b198a63d2363f98bfdd92955614ab
Suraj N. Kurapati <sunaku@gmail.com>
2011-01-27 10:41:54 -0800

    fix removal of bundle/snipMate/snippets in bundle.sh

--- bundle.sh
+++ bundle.sh
@@ -5,9 +5,8 @@ git submodule init
 git submodule status | awk '/^-/ { print $2 }' | xargs -r git submodule update
 git submodule foreach git pull origin master
 
+rm -vrf bundle/snipMate/snippets/ # conflicts with bundle/0-snipMate/snippets/
+
 cd bundle/
 ruby get.rb
 sh doc.sh
-
-# remove because it conflicts with the better snippets in bundle/0-snipMate
-rm -rf bundle/snipMate/snippets
